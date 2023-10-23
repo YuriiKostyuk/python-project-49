13ab2cd624318dfe0b8568dd10d656b471e51538
YuriiKostyuk <y-kostyuk03@mail.ru>
2023-10-23 18:26:12 +0300

    lint

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,16 +4,18 @@ from brain_games.consts import DESCRIPTION_PRIME
 
 
 def is_prime(number):
-    is_prime = 'yes' if (number >= 2 and
-                         all(number % i != 0 for i in
-                             range(2, int(number ** 0.5) + 1))) else 'no'
-    return is_prime
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5 + 1)):
+        if not (number % i):
+            return False
+    return True
 
 
 def game_prime():
     number = random.randint(1, 10)
     question = number
-    correct_answer = is_prime(number)
+    correct_answer = is_prime(number) and 'yes' or 'no'
     return question, str(correct_answer)
 
 
